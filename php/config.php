d8e6de06190ddabd5d5e2ff70e9aa343a624f564
anantyash <92616079+anantyash@users.noreply.github.com>
2024-06-12 13:09:30 +0000

    adding config and logout file in php folder

---
+++ php/config.php
@@ -0,0 +1,18 @@
+<?php 
+ 
+
+
+$sname = "localhost";
+$uname = "root";
+$password = "";
+
+$db_name = "projectplag_db";
+
+$con = mysqli_connect($sname, $uname, $password, $db_name);
+
+if (!$con) {
+	echo "Connection failed!";
+	exit();
+}
+
+?>
